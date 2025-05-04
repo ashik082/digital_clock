2ab48c80b30176c4298a649d8906071d1532e935
Md. Ashikur Rahman <124452698+ashik082@users.noreply.github.com>
2025-05-05 03:06:07 +0600

    updated with date

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const sec = $('.sec');
 const dot = document.querySelectorAll('.dot');
 const ampm = $('.ampm');
 const week = $('.week');
+const day = $('.day');
+const month = $('.month');
+const year = $('.year');
 let showDot = true;
 
 function update() {
@@ -29,6 +32,9 @@ function update() {
     min.textContent = String(m).padStart(2, '0');
     sec.textContent = String(s).padStart(2, '0');
     ampm.textContent = am_pm;
+    day.textContent = String(now.getDate()).padStart(2, '0');
+    month.textContent = String(now.getMonth() + 1).padStart(2, '0');
+    year.textContent = now.getFullYear();
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const todayName = days[now.getDay()];
 
